55c8f19e7e60feef218b4f63003a6613ede39ac4
Matias Fernandez <matiasfernandez_47@hotmail.com>
2020-02-27 15:13:31 -0300

    ViajesBD esta armada pero hay que cambiar los datos que estan comentados

--- WIlonBD/WIlonBD/Class/ViajesBD.cs
+++ WIlonBD/WIlonBD/Class/ViajesBD.cs
@@ -9,10 +9,162 @@ using Wilon;
 
 namespace WIlonBD.Class
 {
-    public class ViajesBD
+    public class ViajesBD : Commons.BaseDAO
     {
 
+        public int Save(Terceros objeto)
+        {
+            StringBuilder wQuery = new StringBuilder();
+            SqlCommand wCmd = new SqlCommand();
+
+            try
+            //{
+            //    wQuery = new StringBuilder("INSERT INTO [Terceros] (");
+            //    wQuery.Append("			[RazonSocial] ");
+            //    wQuery.Append("			,[Documento] ");
+            //    wQuery.Append("			,[CondicionIVA] ");
+            //    wQuery.Append("			,[Direccion] ");
+            //    wQuery.Append("			,[NroCalle] ");
+            //    wQuery.Append("			,[CodigoPostal] ");
+            //    wQuery.Append("			,[Localidad] ");
+            //    wQuery.Append("			,[Provincia] ");
+            //    wQuery.Append("			,[NroTelefono] ");
+            //    wQuery.Append("			,[Celular] ");
+            //    wQuery.Append("			,[Email] ");
+            //    wQuery.Append("			,[CondicionIIBB] ");
+            //    wQuery.Append("			,[NumeroIIBB] ");
+            //    wQuery.Append("			,[CalculaInteres] ");
+            //    wQuery.Append("			,[Tasa] ");
+            //    wQuery.Append("			,[CBU] ");
+            //    wQuery.Append(") VALUES (");
+            //    wQuery.Append("			@RazonSocial ");
+            //    wQuery.Append("			,@Documento ");
+            //    wQuery.Append("			,@CondicionIVA ");
+            //    wQuery.Append("			,@Direccion ");
+            //    wQuery.Append("			,@NroCalle ");
+            //    wQuery.Append("			,@CodigoPostal ");
+            //    wQuery.Append("			,@Localidad ");
+            //    wQuery.Append("			,@Provincia ");
+            //    wQuery.Append("			,@NroTelefono ");
+            //    wQuery.Append("			,@Celular ");
+            //    wQuery.Append("			,@Email ");
+            //    wQuery.Append("			,@CondicionIIBB ");
+            //    wQuery.Append("			,@NumeroIIBB ");
+            //    wQuery.Append("			,@CalculaInteres ");
+            //    wQuery.Append("			,@Tasa ");
+            //    wQuery.Append("			,@CBU ");
+            //    wQuery.Append(")");
+            //    wQuery.Append("SELECT @@identity");
+
+            //    #region Parameters
+
+            //    // RazonSocial
+            //    wCmd.Parameters.Add(new SqlParameter("@RazonSocial", SqlDbType.NVarChar)).Value = objeto.razonSocial;
+
+            //    // Documento
+            //    wCmd.Parameters.Add(new SqlParameter("@Documento", SqlDbType.Int)).Value = objeto.documento;
+
+            //    // CondicionIVA
+            //    if (objeto.condicionIva != 0)
+            //        wCmd.Parameters.Add(new SqlParameter("@CondicionIVA", SqlDbType.Int)).Value = objeto.condicionIva;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@CondicionIVA", SqlDbType.Int)).Value = DBNull.Value;
+
+            //    // Direccion
+            //    wCmd.Parameters.Add(new SqlParameter("@Direccion", SqlDbType.NVarChar)).Value = objeto.direccion;
+
+            //    // NroCalle
+            //    if (objeto.nro != 0)
+            //        wCmd.Parameters.Add(new SqlParameter("@NroCalle", SqlDbType.Int)).Value = objeto.nro;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@NroCalle", SqlDbType.Int)).Value = DBNull.Value;
 
 
+            //    // CodigoPostal
+            //    if (objeto.cp != 0)
+            //        wCmd.Parameters.Add(new SqlParameter("@CodigoPostal", SqlDbType.Int)).Value = objeto.cp;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@CodigoPostal", SqlDbType.Int)).Value = DBNull.Value;
+
+            //    // Localidad
+            //    if (objeto.localidad != 0)
+            //        wCmd.Parameters.Add(new SqlParameter("@Localidad", SqlDbType.Int)).Value = objeto.localidad;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@Localidad", SqlDbType.Int)).Value = DBNull.Value;
+
+            //    // Provincia
+            //    if (objeto.provincia != 0)
+            //        wCmd.Parameters.Add(new SqlParameter("@Provincia", SqlDbType.Int)).Value = objeto.provincia;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@Provincia", SqlDbType.Int)).Value = DBNull.Value;
+
+            //    // NroTelefono
+            //    if (objeto.nroTelefono != string.Empty)
+            //        wCmd.Parameters.Add(new SqlParameter("@NroTelefono", SqlDbType.NVarChar)).Value = objeto.nroTelefono;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@NroTelefono", SqlDbType.NVarChar)).Value = DBNull.Value;
+
+            //    // Celular
+            //    if (objeto.cel != string.Empty)
+            //        wCmd.Parameters.Add(new SqlParameter("@Celular", SqlDbType.NVarChar)).Value = objeto.cel;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@Celular", SqlDbType.NVarChar)).Value = DBNull.Value;
+
+            //    // Email
+            //    if (objeto.email != string.Empty)
+            //        wCmd.Parameters.Add(new SqlParameter("@Email", SqlDbType.NVarChar)).Value = objeto.email;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@Email", SqlDbType.NVarChar)).Value = DBNull.Value;
+
+            //    // CondicionIIBB
+            //    if (objeto.condicionIIBB != 0)
+            //        wCmd.Parameters.Add(new SqlParameter("@CondicionIIBB", SqlDbType.Int)).Value = objeto.condicionIIBB;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@CondicionIIBB", SqlDbType.Int)).Value = DBNull.Value;
+
+            //    // NumeroIIBB
+            //    if (objeto.nroIIBB != string.Empty)
+            //        wCmd.Parameters.Add(new SqlParameter("@NumeroIIBB", SqlDbType.NVarChar)).Value = objeto.nroIIBB;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@NumeroIIBB", SqlDbType.NVarChar)).Value = DBNull.Value;
+
+            //    // CalculaInteres
+            //    wCmd.Parameters.Add(new SqlParameter("@CalculaInteres", SqlDbType.Bit)).Value = objeto.calculaIntereses;
+
+
+            //    // Tasa
+            //    if (objeto.tasa != 0)
+            //        wCmd.Parameters.Add(new SqlParameter("@Tasa", SqlDbType.NVarChar)).Value = objeto.tasa;
+            //    else
+            //        wCmd.Parameters.Add(new SqlParameter("@Tasa", SqlDbType.NVarChar)).Value = DBNull.Value;
+
+            //    // CBU
+            //    wCmd.Parameters.Add(new SqlParameter("@CBU", SqlDbType.NVarChar)).Value = objeto.cbu;
+
+            //    #endregion
+
+                wCmd.CommandText = wQuery.ToString();
+                if (base.mSqlTransaction == null)
+                    wCmd.Connection = base.GetOpenedConection();
+                else
+                {
+                    wCmd.Connection = (SqlConnection)base.mSqlTransaction.Connection;
+                    wCmd.Transaction = (SqlTransaction)base.mSqlTransaction;
+                }
+                wCmd.CommandTimeout = wCmd.Connection.ConnectionTimeout;
+
+                int wRtn = Convert.ToInt32(wCmd.ExecuteScalar());
+
+                return wRtn;
+            }
+            catch (SqlException exp)
+            {
+                throw new Exception(exp.Message);
+            }
+            finally
+            {
+                if (base.mSqlTransaction == null)
+                    base.CloseConection();
+            }
+        }
     }
-}
